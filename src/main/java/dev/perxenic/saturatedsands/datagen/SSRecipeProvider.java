c7e066ee7149809949cbc96d765e763edadf12ec
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-01-31 17:45:39 +0000

    Added recipe provider class

---
+++ src/main/java/dev/perxenic/saturatedsands/datagen/SSRecipeProvider.java
@@ -0,0 +1,20 @@
+package dev.perxenic.saturatedsands.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.CompletableFuture;
+
+public class SSRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    public SSRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries);
+    }
+
+    @Override
+    protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
+
+    }
+}
