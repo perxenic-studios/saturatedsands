9c1240486b048a799d42994bc6ab67de31e36fbc
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-01-31 17:46:24 +0000

    Added data generators class

---
+++ src/main/java/dev/perxenic/saturatedsands/datagen/SSDataGenerators.java
@@ -0,0 +1,36 @@
+package dev.perxenic.saturatedsands.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
+
+@EventBusSubscriber
+public class SSDataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        BlockTagsProvider blockTagsProvider = new SSBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
+        generator.addProvider(event.includeServer(), blockTagsProvider);
+        generator.addProvider(event.includeServer(), new SSItemTagProvider(
+                packOutput,
+                lookupProvider,
+                blockTagsProvider.contentsGetter(),
+                existingFileHelper
+        ));
+        generator.addProvider(event.includeServer(), new SSRecipeProvider(packOutput, lookupProvider));
+
+        generator.addProvider(event.includeClient(), new SSBlockStateProvider(packOutput, existingFileHelper));
+        generator.addProvider(event.includeClient(), new SSItemModelProvider(packOutput, existingFileHelper));
+    }
+}
