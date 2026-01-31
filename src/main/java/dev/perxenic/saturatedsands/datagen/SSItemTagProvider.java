2983b393353eb7c94bbdde3a6b9ad4a09723d248
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-01-31 20:23:32 +0000

    Reordered pattern tags

--- src/main/java/dev/perxenic/saturatedsands/datagen/SSItemTagProvider.java
+++ src/main/java/dev/perxenic/saturatedsands/datagen/SSItemTagProvider.java
@@ -2,6 +2,7 @@ package dev.perxenic.saturatedsands.datagen;
 
 import dev.perxenic.mirage.registry.ModItems;
 import dev.perxenic.saturatedsands.SaturatedSands;
+import dev.perxenic.saturatedsands.content.SSItemTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
@@ -12,7 +13,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
 
-import static dev.perxenic.saturatedsands.content.SSItemTags.*;
+import static dev.perxenic.saturatedsands.content.SSItemTags.Pattern;
 
 public class SSItemTagProvider extends ItemTagsProvider {
     public SSItemTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider,
@@ -22,53 +23,71 @@ public class SSItemTagProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(@NotNull HolderLookup.Provider provider) {
-        tag(SUN_PATTERN).add(
+        tag(Pattern.SUN).add(
                 ModItems.WHITE_FADED_TERRACOTTA.get()
         );
-        tag(MODERN_PATTERN).add(
+        tag(Pattern.MODERN).add(
                 ModItems.LIGHT_GRAY_FADED_TERRACOTTA.get()
         );
-        tag(CROSS_PATTERN).add(
+        tag(Pattern.CROSS).add(
                 ModItems.GRAY_FADED_TERRACOTTA.get()
         );
-        tag(CRAWL_PATTERN).add(
+        tag(Pattern.CRAWL).add(
                 ModItems.BLACK_FADED_TERRACOTTA.get()
         );
-        tag(TARGET_PATTERN).add(
+        tag(Pattern.TARGET).add(
                 ModItems.BROWN_FADED_TERRACOTTA.get()
         );
-        tag(POTION_PATTERN).add(
+        tag(Pattern.POTION).add(
                 ModItems.RED_FADED_TERRACOTTA.get()
         );
-        tag(FISH_PATTERN).add(
+        tag(Pattern.FISH).add(
                 ModItems.ORANGE_FADED_TERRACOTTA.get()
         );
-        tag(FLOWER_PATTERN).add(
+        tag(Pattern.FLOWER).add(
                 ModItems.YELLOW_FADED_TERRACOTTA.get()
         );
-        tag(SPOKE_PATTERN).add(
+        tag(Pattern.SPOKE).add(
                 ModItems.LIME_FADED_TERRACOTTA.get()
         );
-        tag(PLANT_PATTERN).add(
+        tag(Pattern.PLANT).add(
                 ModItems.GREEN_FADED_TERRACOTTA.get()
         );
-        tag(CREEP_PATTERN).add(
+        tag(Pattern.CREEP).add(
                 ModItems.CYAN_FADED_TERRACOTTA.get()
         );
-        tag(GEO_PATTERN).add(
+        tag(Pattern.GEO).add(
                 ModItems.LIGHT_BLUE_FADED_TERRACOTTA.get()
         );
-        tag(FAN_PATTERN).add(
+        tag(Pattern.FAN).add(
                 ModItems.BLUE_FADED_TERRACOTTA.get()
         );
-        tag(BLADE_PATTERN).add(
+        tag(Pattern.BLADE).add(
                 ModItems.PURPLE_FADED_TERRACOTTA.get()
         );
-        tag(ARROW_PATTERN).add(
+        tag(Pattern.ARROW).add(
                 ModItems.MAGENTA_FADED_TERRACOTTA.get()
         );
-        tag(LEAF_PATTERN).add(
+        tag(Pattern.LEAF).add(
                 ModItems.PINK_FADED_TERRACOTTA.get()
         );
+
+        tag(Pattern.ALL)
+                .addTag(Pattern.SUN)
+                .addTag(Pattern.MODERN)
+                .addTag(Pattern.CROSS)
+                .addTag(Pattern.CRAWL)
+                .addTag(Pattern.TARGET)
+                .addTag(Pattern.POTION)
+                .addTag(Pattern.FISH)
+                .addTag(Pattern.FLOWER)
+                .addTag(Pattern.SPOKE)
+                .addTag(Pattern.PLANT)
+                .addTag(Pattern.CREEP)
+                .addTag(Pattern.GEO)
+                .addTag(Pattern.FAN)
+                .addTag(Pattern.BLADE)
+                .addTag(Pattern.ARROW)
+                .addTag(Pattern.LEAF);
     }
 }
