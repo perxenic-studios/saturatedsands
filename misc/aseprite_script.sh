95e56b49c4f56fe159915b8e4be9d91f8a6a7fb6
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-02-14 20:39:24 +0000

    Fixed referring to pos args which no longer exist

--- misc/aseprite_script.sh
+++ misc/aseprite_script.sh
@@ -3,14 +3,14 @@
 # Arguments:
 # $1 - Aseprite executable path
 
-# Loop through every pattern in directory specified by positional argument 2
+# Loop through every pattern
 find './pattern' | while read pattern; do
     # Check if pattern is a file
     if ! [ -f "$pattern" ]; then
         continue
     fi
 
-    # Loop through every palette in directory specified by positional argument 3
+    # Loop through every palette
     find './palette' | while read palette; do
         # Check if palette is a file
         if ! [ -f "$palette" ]; then
